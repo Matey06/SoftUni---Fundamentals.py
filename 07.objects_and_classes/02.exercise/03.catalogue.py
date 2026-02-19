2bc16ef030b8a38ea10c2ae460f415e1f5f360cb
Matey06 <matey.mateev2006@gmail.com>
2026-02-19 18:45:42 +0200

    some exercises

---
+++ 07.objects_and_classes/02.exercise/03.catalogue.py
@@ -0,0 +1,22 @@
+class Catalogue:
+    def __init__(self, name):
+        self.name = name
+        self.products = []
+
+
+    def add_product(self, product_name: str):
+        self.products.append(product_name)
+
+
+    def get_by_letter(self, first_letter: str):
+        for current_product in self.products:
+            if current_product[0] != first_letter:
+                self.products.remove(current_product)
+
+        return self.products
+
+
+    def __repr__(self):
+        self.products.sort()
+        self.formated = '\n'.join(self.products)
+        return f"Items in the {self.name} catalogue:\n{self.formated}"
